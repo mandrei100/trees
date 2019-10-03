9a7ba0aab9459a53bbafe130bdfb8312734f2db4
mandrei100 <mcrisandreica@hotmail.com>
2019-10-02 21:53:07 -0400

    half done

--- fundamentals/trees/trees.js
+++ fundamentals/trees/trees.js
@@ -2,7 +2,32 @@
 const root = document.getElementById('root');
 
 const ourTree = {
-  // YOUR CODE
+
+  div: {
+    style: [
+      'background-color:red',
+      'width',
+      'min-height',
+      'border',
+    ],
+    redBoxChildren: ['BUTTON', 'DIV', 'SPAN']
+  }
+  
+
+
+  // div: {
+    //   header: "Hello There",
+    //   button: "Click me plz",
+    //   div: {
+    //     header: "I'm a green box",
+    //     div: {
+    //       div: {},
+    //       div: {},
+    //       div: {}
+    //     }
+    //   }
+    // }
+
 };
 
 const DOMRenderer = {
@@ -14,7 +39,44 @@ const DOMRenderer = {
 
 // The function that uses the rendering API object above (DOMRenderer) to build the page as expected!
 const treeMaker = (soil, tree, renderer) => {
-  // YOUR CODE
+  // redbox DIV
+  const rootOfTree = Object.keys(tree)[0];
+  // console.log(rootOfTree);
+  const createdRootOfTree = renderer.create(rootOfTree);
+  console.log("Created the Box", createdRootOfTree);
+
+  const redBoxChildren = tree.div.redBoxChildren;
+  // console.log(redBoxChildren[0]);
+  const rbButton = renderer.create(redBoxChildren[0]);
+  // console.log(rbButton);
+  const rbString = "Click Me Please";
+  
+  const rbStringCreated = renderer.createText(rbString);
+  const renderedText = renderer.append(rbButton, rbStringCreated);
+  const rbSpan = renderer.create(redBoxChildren[2]);
+
+
+  console.log("red box span with button", rbSpan);
+  
+  // Adding String
+  
+  const RedString = "Hello There";
+  const RedStringCreated = renderer.createText(RedString);
+  console.log("created red string", RedStringCreated);
+
+  const rbButtonSpan = renderer.append(rbSpan, rbButton);
+
+
+  const rbSpanTwo = renderer.create(redBoxChildren[2]);
+  const secondSpanAppended = renderer.append(rbSpanTwo, RedStringCreated);
+  const rbStringSpan = renderer.append(createdRootOfTree, secondSpanAppended);
+
+
+  createdRootOfTree.style['background-color'] = 'red';
+  createdRootOfTree.style.height = '100px';
+  createdRootOfTree.style.width = '100%';
+
+  renderer.append(soil, createdRootOfTree);
 };
 
 // DONT TOUCH BELOW
